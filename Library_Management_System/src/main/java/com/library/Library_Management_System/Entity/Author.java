49424ce1b31152f7e667ef820972d607a5380722
kapoormayank9696 <kapoormayank645@gmail.com>
2026-06-24 18:06:44 +0530

    Library Management Project Files Create Successfully

--- Library_Management_System/src/main/java/com/library/Library_Management_System/Entity/Author.java
+++ Library_Management_System/src/main/java/com/library/Library_Management_System/Entity/Author.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class Author {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "author_id")
